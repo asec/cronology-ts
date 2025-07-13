5131f19d142211288c18fef4b61f552673619427
asec <zsamboki.roland@gmail.com>
2025-07-13 20:18:55 +0200

    New commit to test pull requests

--- src/api.ts
+++ src/api.ts
@@ -1,6 +1,7 @@
 import services from "./services/index.js";
 import commandList from "./cli/commands/index.js";
 
+// Entry point for the application
 const cli = services.resolve("cli");
 await cli.init(commandList);
 await cli.start();
