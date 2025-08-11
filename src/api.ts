c2d341a2b6c5d56bca9904b6c3dc6cc40a501f5e
asec <zsamboki.roland@gmail.com>
2025-08-11 23:25:30 +0200

    test-newer modification-2

--- src/api.ts
+++ src/api.ts
@@ -1,7 +1,7 @@
 import services from "./services/index.js";
 import commandList from "./cli/commands/index.js";
 
-// TEST
+// TEST 2
 const cli = services.resolve("cli");
 await cli.init(commandList);
 await cli.start();
